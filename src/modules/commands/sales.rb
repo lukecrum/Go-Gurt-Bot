e68b5d1e1195bef75a7d69355c5571ecbd8fc023
lukecrum20 <lukecrum20@gmail.com>
2018-07-27 00:38:37 -0400

    epic stock command

---
+++ src/modules/commands/sales.rb
@@ -0,0 +1,22 @@
+require 'stock-markit'
+module Bot::DiscordCommands
+  # Responds with "Pong!".
+  # This used to check if bot is alive
+  module Sales
+    extend Discordrb::Commands::CommandContainer
+    command :sales do |_event|
+      percent = StockMarkit.quote(:gis).change_percent
+      puts "#{percent.to_s}"
+      "Gogurt stock #{up_or_down(percent)} #{percent.to_s}%"
+    end
+    def self.up_or_down(change)
+      if change > 0
+        'is up'
+      elsif change < 0
+        'is down'
+      else
+        'is the same:'
+      end
+    end
+  end
+end
